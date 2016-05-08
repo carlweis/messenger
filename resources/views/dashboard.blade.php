b6876afbfab5415a14d36e83539ead5958258fba
carlweis <carl@carlweis.com>
2016-05-08 18:32:21 -0500

    Add space to dashboard

--- resources/views/dashboard.blade.php
+++ resources/views/dashboard.blade.php
@@ -3,6 +3,7 @@
 @section('content')
 <div class="container">
 	<div class="row">
+	<br><br>
 		<div class="col-md-8">
 			<user-directory :users.sync="users" :user.sync="user"></user-directory>
 		</div>
